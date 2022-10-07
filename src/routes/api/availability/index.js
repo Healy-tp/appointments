60df4d73074ec57fb2f10c0458f10be4c2146aee
Hernan Tain <hernantain@gmail.com>
2022-10-07 15:45:55 -0300

    Using error handler middleware in routes

--- src/routes/api/availability/index.js
+++ src/routes/api/availability/index.js
@@ -13,54 +13,56 @@ router.get('/:doctorId', getAvailabilityByDoctorId);
 
 module.exports = router;
 
-function createAvailability(req, res) {
+async function createAvailability(req, res, next) {
+  try {
   // Probably userId will be retrieved from currentUser or another middleware
-  const {
-    doctorId,
-    officeId,
-    weekday,
-    startHour,
-    endHour,
-    frequency,
-    validUntil,
-  } = req.body;
+    const {
+      doctorId,
+      officeId,
+      weekday,
+      startHour,
+      endHour,
+      frequency,
+      validUntil,
+    } = req.body;
 
-  if (!doctorId || !officeId || !weekday || !startHour || !endHour || !frequency || !validUntil) {
-    return res.status(422).send({ message: 'You are missing required fields.' });
+    if (!doctorId || !officeId || !weekday || !startHour || !endHour || !frequency || !validUntil) {
+      return res.status(422).send({ message: 'You are missing required fields.' });
+    }
+
+    const response = await availabilityController.createAvailability({
+      doctorId,
+      officeId,
+      weekday,
+      startHour,
+      endHour,
+      frequency,
+      validUntil,
+    });
+    res.status(200).send(response);
+  } catch (err) {
+    logger.error(err.message);
+    next(err);
   }
-
-  return availabilityController.createAvailability({
-    doctorId,
-    officeId,
-    weekday,
-    startHour,
-    endHour,
-    frequency,
-    validUntil,
-  })
-    .then((data) => res.status(201).send({ data }))
-    .catch((error) => {
-      logger.error(error.message);
-      res.status(500).send({ message: error.message });
-    });
 }
 
-function getAvailabilityByDoctorId(req, res) {
-  const doctorId = _.get(req, 'params.doctorId');
-
-  return availabilityController.getByDoctorId(doctorId)
-    .then((data) => res.status(200).send({ data }))
-    .catch((error) => {
-      logger.error(error.message);
-      res.status(500).send({ message: error.message });
-    });
+async function getAvailabilityByDoctorId(req, res, next) {
+  try {
+    const doctorId = _.get(req, 'params.doctorId');
+    const response = await availabilityController.getByDoctorId(doctorId);
+    res.status(200).send(response);
+  } catch (err) {
+    logger.error(err.message);
+    next(err);
+  }
 }
 
-function getAllRecords(req, res) {
-  return availabilityController.getAllRecords()
-    .then((data) => res.status(200).send({ data }))
-    .catch((error) => {
-      logger.error(error.message);
-      res.status(500).send({ message: error.message });
-    });
+async function getAllRecords(req, res, next) {
+  try {
+    const response = await availabilityController.getAllRecords();
+    res.status(200).send(response);
+  } catch (err) {
+    logger.error(err.message);
+    next(err);
+  }
 }
